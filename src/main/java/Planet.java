dc6d138912c079a1fc93c90547f37d606ab030d9
Connor <connorjrobertson@hotmail.co.uk>
2021-08-23 12:56:21 +0100

    Commentary added

--- src/main/java/Planet.java
+++ src/main/java/Planet.java
@@ -1,26 +1,36 @@
-public class Planet {
+public class Planet { //Planet Class
 
-    private String name;
-    private Integer size;
+    private String name; //Planet class has a Private name variable which is string, Private means it can only be changed by interacting with the planet class
+    private Integer size; //Planet class has a Private size variable which is an Integer, Private means it can only be changed by interacting with the planet class
 
+    //Planet constructor (how a Planet object is made - what arguments does it need), We've decided it needs a name (which must be a string) and a size (which must be an Integer)
     public Planet(String name, Integer size){
-        this.name = name;
-        this.size = size;
+        this.name = name; //When a planet object is created, it's name variable is set to the name String given as an argument
+        this.size = size; //When a planet object is created, it's size variable is set to the size Integer given as an argument
     }
 
-    public String getName(){
-        return this.name;
+    public String getName(){ // We've created a method that can be used with a Planet object, it's called getName and it will return a string
+        return this.name; // The return value of the getName method is the name string value of whichever planet the .getName() method was called on
     }
 
-    public Integer getSize(){
-        return this.size;
+    public Integer getSize(){ // We've created a method that can be used with a Planet object, it's called getSize and it will return an Integer
+        return this.size; // The return value of the getSize method is the size Integer value of whichever planet the .getSize() method was called on
     }
 
-    public String explode(){
+    public String explode(){ // We've created a method that can be used with a Planet object, it's called explode and it will return a String we've defined below
         return String.format("Boom! %s has exploded! It was %s in size!", this.name, this.size);
+        //The above string is what will be returned when the explode method is called on a planet object
+            // The .format() method is being used on this string so we can input variables into the String
+                // Wherever a %s exists in the string, it will be replaced with the variable following the string
+                    // so the first %s in the above will become whatever the value of this.name is
+                    // and the second %s in the above will become whatever the value of this.size is
+                    // so if we were using this on a planet that had this.name = Earth and this.size = 100000 the string would be
+                        // Boom! Earth has exploded! It was 100000 in size!"
+                            // This is the string that will be returned by the explode method
     }
-
-    public void setName(String name){
-        this.name = name;
+    // We've created a method that can be used with a Planet object, it's called setName and it will be used to modify the name of the planet
+    // we've used void because this method does not 'return' anything
+    public void setName(String name){ // When setName is called on a planet object, it requires a string to be given as an argument, which will be treated as 'name'
+        this.name = name; // When this method runs, it takes whatever String it was given when it was called and updates the name of the planet the method was called on to be the new name given
     }
 }
--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,13 +1,32 @@
 public class Runner {
 
     public static void main(String[] args) {
-        Planet planet = new Planet("Earth", 90000);
-        String name = planet.getName();
-        Integer size = planet.getSize();
-        System.out.println(name);
-        System.out.println(size);
-        planet.setName("Mars");
-        String message = planet.explode();
-        System.out.println(message);
+        // We've created a planet object, called it ourPlanet and given it the String "Earth" to be used as name, and the Integer 90000 to be used as size
+        Planet ourPlanet = new Planet("Earth", 90000);
+
+        // We want to check that the name has been set to Earth as expected
+        // So we set up a String name variable here and assign it to the return value of the getName method when called on ourPlanet
+        String name = ourPlanet.getName(); // the value of name within Runner should now be 'Earth'
+
+        // We want to check that the size has been set to 90000 as expected
+        // So we set up a Integer size variable here and assign it to the return value of the getSize method when called on ourPlanet
+        Integer size = ourPlanet.getSize(); // the value of size within Runner should now be '90000'
+
+        //We want to print the value of the name variable (which was defined as the result of outPlanet.getName() on line 9
+        System.out.println(name); // So we do the print command and give it (name), so when runner is run 'Earth' should be printed to the console
+
+        //We want to print the value of the size variable (which was defined as the result of outPlanet.getSize() on line 13
+        System.out.println(size); // So we do the print command and give it (size), so when runner is run '90000' should be printed to the console
+
+        //We want to change the name of ourPlanet to be Mars using the setName method we created in Planet.java
+        ourPlanet.setName("Mars"); // We call setName on ourPlanet and give it the string "Mars", so the name value of ourPlanet should now be "Mars"
+
+        // Our explode method returns a string and uses the current name and size variables to fill in some gaps
+            // Before we changed the name value of ourPlanet() to be Mars, we'd have expected the ourPlanet.explode to return "Boom! Earth has exploded! It was 90000 in size!"
+                // However on line 22 we changed the name value of ourPlanet to be "Mars"
+        String message = ourPlanet.explode(); //We create a String message variable within the runner file and assign it to the returned value of ourPlanet.explode()
+
+        // Now we actually want to print the message from line 27, so we use the print command and give it message as an argument
+        System.out.println(message); //When the runner file is run, the console should print the String value of message, which should be "Boom! Mars has exploded! It was 90000 in size!"
     }
 }
